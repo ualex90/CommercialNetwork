e2939d9b39af695a626b96729a49240732c66822
u_alex90 <u_alex90@mail.ru>
2024-02-24 20:50:44 +1000

    Create commercial models

--- commercial/models.py
+++ commercial/models.py
@@ -1,3 +1,54 @@
+from django.utils.translation import gettext_lazy as _
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Product(models.Model):
+    """ Продукт """
+    name = models.CharField(max_length=300, verbose_name=_("Name"))
+    model = models.CharField(max_length=300, verbose_name=_("Model"))
+    release_date = models.DateTimeField(auto_now_add=True, verbose_name=_("Release Date"))
+
+
+class Contacts(models.Model):
+    """ Контакты """
+    email = models.EmailField(verbose_name=_("Email"))
+    country = models.CharField(max_length=300, verbose_name=_("Country"))
+    city = models.CharField(max_length=300, verbose_name=_("City"))
+    street = models.CharField(**NULLABLE, max_length=300, verbose_name=_("Street"))
+    house = models.CharField(**NULLABLE, max_length=10, verbose_name=_("House"))
+
+
+FACTORY = "FA"
+INDIVIDUAL_ENTREPRENEUR = "IE"
+
+# Выбор типа поставщика (Завод или индивидуальный предприниматель)
+PROVIDER_TYPE_CHOICES = [
+        (FACTORY, _("Factory")),
+        (INDIVIDUAL_ENTREPRENEUR, _("Individual Entrepreneur")),
+    ]
+
+
+class BaseModel(models.Model):
+    """ Базовая модель """
+    name = models.CharField(max_length=300, verbose_name=_("Name"))
+    contacts = models.ForeignKey(Contacts, **NULLABLE, on_delete=models.CASCADE, verbose_name=_("Contacts"))
+    arrears = models.DecimalField(**NULLABLE, max_digits=12, max_length=2, verbose_name=_("Arrears"))
+    creation_time = models.DateTimeField(auto_now_add=True, verbose_name=_("Creation Time"))
+
+
+class Provider(BaseModel):
+    """ Поставщик """
+    provider_type = models.CharField(max_length=2, choices=PROVIDER_TYPE_CHOICES, verbose_name=_("Provider Type"))
+    products = models.ManyToManyField(Product, verbose_name=_("Products"))
+    provider = models.ManyToManyField(
+        'self', **NULLABLE, on_delete=models.CASCADE, related_name='customer', verbose_name=_("Provider")
+    )
+
+
+class RetailNetwork(BaseModel):
+    """ Розничная сеть """
+    provider = models.ManyToManyField(
+        Provider, **NULLABLE, on_delete=models.CASCADE, related_name='customer', verbose_name=_("Provider")
+    )
