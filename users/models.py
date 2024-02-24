109927ac72ef2dc80aa754cf56d68ade59584476
u_alex90 <u_alex90@mail.ru>
2024-02-24 20:50:26 +1000

    mod User model

--- users/models.py
+++ users/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 from django.db import models
 
 NULLABLE = {'blank': True, 'null': True}
@@ -7,7 +8,7 @@ NULLABLE = {'blank': True, 'null': True}
 class User(AbstractUser):
     username = None
 
-    email = models.EmailField(unique=True, verbose_name='Email')
+    email = models.EmailField(unique=True, verbose_name=_("Email"))
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
